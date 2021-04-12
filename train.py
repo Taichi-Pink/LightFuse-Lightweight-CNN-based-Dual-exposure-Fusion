7d4f94d4cc26c091bc97fe55f8509a69bf2b26dc
Taichi <15868881759@163.com>
2021-04-12 21:51:30 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,53 +1,48 @@
 import tensorflow as tf
 tf.enable_eager_execution()
-import os
-os.environ["CUDA_VISIBLE_DEVICES"]="1,2"
-import numpy as np
-from utils import *
-from load_data import load_data
 import matplotlib.pyplot as plt
 import math, cv2, glob, time
+import numpy as np
 import imageio as io
+from load_data import load_data
+from utils import *
+import os
 
 patch_size = 256
-bs = 1
-def train(model, indexx):
+epochs     = 80
+bs         = 1
+ch         = 6
+
+def train(model):
   dataset = load_data(bs, patch_size) 
   loss_history = []
   print(model.summary())
-  #model.load_weights('./model1_3.h5')
   
-  for epoch in range(0,2):
+  for epoch in range(epochs):
       loss1 = []
-      step=0
+      step  = 0
       
       for ldr, hdr in dataset:
-          ldr = tf.image.resize_images(tf.cast(ldr, tf.float32), (256,256))*2.0-1.0
-          hdr = tf.image.resize_images(tf.cast(hdr, tf.float32), (256,256))*2.0-1.0
+          ldr = tf.image.resize_images(tf.cast(ldr, tf.float32), (patch_size, patch_size))*2.0-1.0
+          hdr = tf.image.resize_images(tf.cast(hdr, tf.float32), (patch_size, patch_size))*2.0-1.0
           
-          c1 = model.train_on_batch(ldr, hdr)
-          loss1.append(c1)       
-          #print('Epoch:%d, step:%d, model1:%f'%(epoch, step, c1))
+          l = model.train_on_batch(ldr, hdr)
+          loss1.append(l)       
+          print('epoch:%d, step:%d, model_loss:%f'%(epoch, step, l))
           step = step+1                
-      model.save_weights('./model'+str(indexx)+'_'+ str(epoch) +'.h5')
+      
       
       loss_mean = np.mean(loss1)
-      loss_history.append(loss_mean)
-      print(loss_mean)
+      loss_history.append(loss_mean)  
       
   fig = plt.figure()
   plt.plot(loss_history)
-  fig.savefig('temp'+str(indexx)+'.png', dpi=fig.dpi)
-  
+  fig.savefig('loss_history.png', dpi=fig.dpi)
+  model.save_weights('./lightfuse_model_weights.h5')
                                              
 if __name__ == "__main__":
-  for i in range(1,2):
-    if i == 1:
-      model = model_test1(256,256,6)
-    elif i == 2:
-      model = model_test2(256,256,6)
-      
-    train(model, i)
+    model = supervised_model(patch_size, patch_size, ch)
+    train(model)
    
 
       
