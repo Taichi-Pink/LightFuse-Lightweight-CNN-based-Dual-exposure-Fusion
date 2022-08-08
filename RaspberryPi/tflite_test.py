2a78a5c7dea50554f53018cf480e72199148288d
Taichi <15868881759@163.com>
2022-08-08 15:39:30 -0700

    Add files via upload

--- RaspberryPi/tflite_test.py
+++ RaspberryPi/tflite_test.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
+#tf.enable_eager_execution()
 from tensorflow import keras
 from tensorflow.keras import layers, Model
-import cv2
+# import matplotlib.pyplot as plt
+import cv2, time
 import numpy as np
 import imageio as io
 ch = 6
-w  = 336
-h  = 224
+w = 336#1344#256
+h = 224#896#128
 
 def norm_0_to_1(img):
     img = np.float32(img)
@@ -18,7 +20,8 @@ def norm_0_to_1(img):
 
 def test_tflite():
     # Load TFLite model and allocate tensors.
-    interpreter = tf.lite.Interpreter(model_path="lite_model.tflite")
+    path_p = 'test_cust_s56.png'#change
+    interpreter = tf.lite.Interpreter(model_path="lite_model_cust_336x224_no_tile.tflite")#change
     interpreter.allocate_tensors()
 
     # Get input and output tensors.
@@ -26,11 +29,11 @@ def test_tflite():
     output_details = interpreter.get_output_details()
 
     # load data
-    p_over = './test/1/over_exposed.jpg'
+    p_over = '1.jpg'#'./over_exp256x128.png'
     over_exp = cv2.imread(p_over)
     over_exp = over_exp[:, :, ::-1]
 
-    p_under = './test/1/under_exposed.jpg'
+    p_under = '2.jpg'#'./under_exp256x128.png'
     under_exp = cv2.imread(p_under)
     under_exp = under_exp[:, :, ::-1]
 
@@ -42,21 +45,27 @@ def test_tflite():
     img = np.concatenate([under_exp, over_exp], axis=2)
     img = np.expand_dims(img, axis=0)
     img = img * 2.0 - 1.0
-
-    # Test model on random input data.
-    interpreter.set_tensor(input_details[0]['index'], img)
-
-    interpreter.invoke()
-
-    # The function `get_tensor()` returns a copy of the tensor data.
-    # Use `tensor()` in order to get a pointer to the tensor.
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-
-    ldr = np.squeeze(output_data)
-    ldr_ = ((ldr + 1.0) / 2.0)
-    tem = ldr_ * 255.0
-    tem = tem.astype(np.uint8)
-    io.imwrite('test_cust.png', tem)
+    
+    t_start = time.time()
+    print('start:')
+    for i in range(60):
+    
+      # Test model on random input data.
+      interpreter.set_tensor(input_details[0]['index'], img)
+      interpreter.invoke()
+  
+      # The function `get_tensor()` returns a copy of the tensor data.
+      # Use `tensor()` in order to get a pointer to the tensor. 
+      output_data = interpreter.get_tensor(output_details[0]['index'])
+    
+    print('time:')
+    print((time.time()-t_start)/60.0)
+    
+    #ldr = np.squeeze(output_data)
+    #ldr_ = ((ldr + 1.0) / 2.0)
+    #tem = ldr_ * 255.0
+    #tem = tem.astype(np.uint8)
+    #io.imwrite(path_p, tem)
 
 
 if __name__ == "__main__":
